b1424caeb19be78a4652335ad6d5af8cfddfb2dd
Paudel3101 <bishalsharma24112002@gmail.com>
2026-03-02 19:03:44 -0500

    Initial Commit

---
+++ DidITakeIt Watch App/DidITakeItApp.swift
@@ -0,0 +1,17 @@
+//
+//  DidITakeItApp.swift
+//  DidITakeIt Watch App
+//
+//  Created by Bishal Paudel on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct DidITakeIt_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DidITakeIt/DidITakeItApp.swift
@@ -0,0 +1,17 @@
+//
+//  DidITakeItApp.swift
+//  DidITakeIt
+//
+//  Created by Bishal Paudel on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct DidITakeItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
